668720bb664f0b8135f8d4ad9a15cfe96238835f
Kevin Arbelaez <kevinrestrepo08@gmail.com>
2024-11-09 11:40:10 -0800

    Replaced BrowserRouter with HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import './App.scss';
-import Home from './components/home/home';
-import TopNav from './components/top-nav/top-nav';
+import React from "react";
+import { Route, HashRouter as Router, Routes } from "react-router-dom";
+import "./App.scss";
+import Home from "./components/home/home";
+import TopNav from "./components/top-nav/top-nav";
 
 const navItems = [
-  { label: 'About us', href: '/about' },
-  { label: 'Contact', href: '/contact' },
-  { label: 'Services', href: '/services' },
+  { label: "About us", href: "/about" },
+  { label: "Contact", href: "/contact" },
+  { label: "Services", href: "/services" },
 ];
 
 const App: React.FC = () => {
