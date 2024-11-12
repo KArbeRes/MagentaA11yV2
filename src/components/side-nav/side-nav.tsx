08e01392641fe32f2b37ebff1f81eead08760609
Kevin Arbelaez <kevinrestrepo08@gmail.com>
2024-11-11 19:15:32 -0800

    fixed side nav keyboard navigation. Hiding sub links from screenreader and keyboard when parent is collapsed

--- src/components/side-nav/side-nav.tsx
+++ src/components/side-nav/side-nav.tsx
@@ -1,10 +1,18 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import contentData from "../../shared/content.json";
 import "./side-nav.scss";
 
 const SideNav: React.FC = () => {
-  const renderNavItems = (items: any[], isTopLevel = false) => (
+  const location = useLocation();
+
+  const isActive = (path: string) => location.pathname.includes(path);
+
+  const renderNavItems = (
+    items: any[],
+    parentActive = false,
+    isTopLevel = false
+  ) => (
     <ul
       className={
         isTopLevel
@@ -12,47 +20,51 @@ const SideNav: React.FC = () => {
           : "MagentaA11y__side-nav--sub-list"
       }
     >
-      {items.map((item) => (
-        <li
-          key={item.name}
-          className={
-            isTopLevel
-              ? "MagentaA11y__side-nav--item"
-              : "MagentaA11y__side-nav--sub-item"
-          }
-        >
-          <NavLink
-            to={item.name}
-            className={`MagentaA11y__side-nav--link ${
-              !isTopLevel ? "MagentaA11y__side-nav--sub-link" : ""
-            }`}
+      {items.map((item) => {
+        const itemActive = isActive(item.name);
+        const activeState = parentActive || itemActive;
+
+        return (
+          <li
+            key={item.name}
+            className={
+              isTopLevel
+                ? "MagentaA11y__side-nav--item"
+                : "MagentaA11y__side-nav--sub-item"
+            }
           >
-            {item.label}
-            {/* Only render SVG icon for top-level items */}
-            {isTopLevel && (
-              <svg
-                slot="icon"
-                xmlns="http://www.w3.org/2000/svg"
-                height="24px"
-                viewBox="0 -960 960 960"
-                width="24px"
-              >
-                <path d="M480-360 280-560h400L480-360Z" />
-              </svg>
-            )}
-          </NavLink>
-          {/* Recursively render child directories, if any */}
-          {item.children &&
-            item.children.length > 0 &&
-            renderNavItems(item.children, false)}
-        </li>
-      ))}
+            <NavLink
+              to={item.name}
+              className={`MagentaA11y__side-nav--link ${
+                !isTopLevel ? "MagentaA11y__side-nav--sub-link" : ""
+              }`}
+              {...(!isTopLevel && !activeState && { tabIndex: -1 })}
+            >
+              {item.label}
+              {isTopLevel && (
+                <svg
+                  slot="icon"
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="24px"
+                  viewBox="0 -960 960 960"
+                  width="24px"
+                >
+                  <path d="M480-360 280-560h400L480-360Z" />
+                </svg>
+              )}
+            </NavLink>
+            {item.children &&
+              item.children.length > 0 &&
+              renderNavItems(item.children, activeState, false)}
+          </li>
+        );
+      })}
     </ul>
   );
 
   return (
     <div className="MagentaA11y__side-nav">
-      {renderNavItems(contentData, true)}
+      {renderNavItems(contentData, false, true)}
       <md-filled-button>Collapse All</md-filled-button>
     </div>
   );
