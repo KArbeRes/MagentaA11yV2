12f1c58ba504c79cb869be2c044519688448a65c
Kevin Arbelaez <kevinrestrepo08@gmail.com>
2024-11-11 18:04:08 -0800

    Adjusted navigation statuses and added BEm classes to ul and li

--- src/components/side-nav/side-nav.tsx
+++ src/components/side-nav/side-nav.tsx
@@ -1,18 +1,32 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import contentData from '../../shared/content.json';
-import './side-nav.scss';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import contentData from "../../shared/content.json";
+import "./side-nav.scss";
 
 const SideNav: React.FC = () => {
   const renderNavItems = (items: any[], isTopLevel = false) => (
-    <ul>
+    <ul
+      className={
+        isTopLevel
+          ? "MagentaA11y__side-nav--list"
+          : "MagentaA11y__side-nav--sub-list"
+      }
+    >
       {items.map((item) => (
-        <li key={item.name}>
+        <li
+          key={item.name}
+          className={
+            isTopLevel
+              ? "MagentaA11y__side-nav--item"
+              : "MagentaA11y__side-nav--sub-item"
+          }
+        >
           <NavLink
             to={item.name}
             className={`MagentaA11y__side-nav--link ${
-              !isTopLevel ? 'MagentaA11y__side-nav--sub-link' : ''
-            }`}>
+              !isTopLevel ? "MagentaA11y__side-nav--sub-link" : ""
+            }`}
+          >
             {item.label}
             {/* Only render SVG icon for top-level items */}
             {isTopLevel && (
@@ -21,7 +35,8 @@ const SideNav: React.FC = () => {
                 xmlns="http://www.w3.org/2000/svg"
                 height="24px"
                 viewBox="0 -960 960 960"
-                width="24px">
+                width="24px"
+              >
                 <path d="M480-360 280-560h400L480-360Z" />
               </svg>
             )}
