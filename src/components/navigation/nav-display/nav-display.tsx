e21dae4211727960e547d44bac85189ebfeb13fe
Kevin Arbelaez <kevinrestrepo08@gmail.com>
2024-12-13 15:07:29 -0800

    updated cards styles

--- src/components/navigation/nav-display/nav-display.tsx
+++ src/components/navigation/nav-display/nav-display.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/no-redundant-roles */
 import React, { useEffect, useRef, useState } from "react";
 import { useLocation, NavLink } from "react-router-dom";
 import ReactMarkdown from "react-markdown";
@@ -224,9 +225,13 @@ const NavDisplay: React.FC<NavDisplayProps> = ({ platform, items }) => {
 
       {/* Sub-items list */}
       {children && children.length > 0 && (
-        <ul className="MagentaA11y__nav-display--sub-list">
+        <ul className="MagentaA11y__nav-display--sub-list" role="list">
           {children.map((child) => (
-            <li key={child.name} className="MagentaA11y__nav-display--sub-item">
+            <li
+              key={child.name}
+              className="MagentaA11y__nav-display--sub-item"
+              role="listitem"
+            >
               <NavLink
                 to={`${location.pathname}/${child.name}`}
                 className="MagentaA11y__nav-display--sub-item-link"
@@ -239,6 +244,18 @@ const NavDisplay: React.FC<NavDisplayProps> = ({ platform, items }) => {
                     {child.generalNotes}
                   </span>
                 )}
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M12 19.6155L10.9308 18.5616L16.7424 12.7501H4.38465V11.2501H16.7424L10.9308 5.43859L12 4.38477L19.6154 12.0001L12 19.6155Z"
+                    fill="#121212"
+                  />
+                </svg>
               </NavLink>
             </li>
           ))}
