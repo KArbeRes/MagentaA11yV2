b3dc71839452c9dbd6d7ab30f8d57ca1e79f5d5c
Kevin Arbelaez <kevinrestrepo08@gmail.com>
2025-01-13 17:24:33 -0800

    removed unused var

--- src/components/navigation/side-nav/side-nav.tsx
+++ src/components/navigation/side-nav/side-nav.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import contentData from "../../../shared/content.json";
 import { useViewport } from "../../../shared/contexts/viewport-context";
