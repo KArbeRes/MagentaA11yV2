3181a328d7b5befa4a740373f660f2077bedf992
KArbela <kevin.arbelaez@t-mobile.com>
2025-01-17 11:26:50 -0800

    Refactored styles and components to enhance consistency and maintainability. Updated SCSS by replacing hardcoded padding with the --content-details-padding variable, unifying font settings with the addition of --magentaa11y-typeset-headline-sm-strong, and standardizing quotation marks to single quotes. Adjusted button and navigation element styles for alignment improvements. Refined @font-face declarations to use single quotes and consistent formatting. Streamlined the TopNav component with improved JSX formatting and consistent attribute handling, ensuring better readability and adherence to the design system.

--- src/components/navigation/top-nav/top-nav.tsx
+++ src/components/navigation/top-nav/top-nav.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
-import { Icons } from "shared/Icons";
-import { useViewport } from "../../../shared/contexts/viewport-context";
-import IconButton from "../../custom-components/buttons/icon-button/icon-button";
-import { TopNavProps } from "../nav.types";
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { Icons } from 'shared/Icons';
+import { useViewport } from '../../../shared/contexts/viewport-context';
+import IconButton from '../../custom-components/buttons/icon-button/icon-button';
+import { TopNavProps } from '../nav.types';
 
-import "./top-nav.scss";
+import './top-nav.scss';
 
 const TopNav: React.FC<TopNavProps> = ({ navItems }) => {
   const viewportContext = useViewport();
@@ -26,28 +26,25 @@ const TopNav: React.FC<TopNavProps> = ({ navItems }) => {
 
       {viewportContext.isMobile && (
         <IconButton
-          a11yLabel={"Menu Button"}
+          a11yLabel={'Menu Button'}
           icon={expanded ? Icons.closeOutlined : Icons.menu}
           ariaExpanded={expanded}
           ariaHasPopup={true}
           ariaControls="top-navigation"
-          onClick={handleMenuClick}
-        ></IconButton>
+          onClick={handleMenuClick}></IconButton>
       )}
 
       <nav
         className="MagentaA11y__navbar__nav"
         aria-label="Top navigation"
-        id="top-navigation"
-      >
+        id="top-navigation">
         <ul className="MagentaA11y__nav-items">
           {navItems.map((item, index) => (
             <li key={index} className="MagentaA11y__nav-items--item">
               <NavLink
                 to={item.href}
                 className="MagentaA11y__nav-items--link"
-                aria-label={`Navigate to ${item.label}`}
-              >
+                aria-label={`Navigate to ${item.label}`}>
                 {item.icon && (
                   <span className="MagentaA11y__nav-items--icon">
                     {item.icon}
