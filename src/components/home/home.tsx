c4e135533b37e3242c12ca7e4fa0a4ebfd8b8022
KArbela <kevin.arbelaez@t-mobile.com>
2025-02-07 17:12:08 -0800

    Updated App.scss by renaming MagentaA11y__content to MagentaA11y--content. Updated App.tsx to reflect this class name change. Added a new brand logo SVG file under assets/svgs. Introduced a new home.scss file with styles for the home page, defining layout variables and responsive styles. Updated home.tsx to include a new header section with a title and logo. Modified top-nav.tsx to import ButtonSize and apply a small size to the menu button. Extended _fonts.scss by adding new typography tokens for various text sizes and weights.

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,9 +1,20 @@
-// src/components/Home.tsx
 import React from 'react';
+import BrandLogo from '../../assets/svgs/brand-logo-magenta.svg';
+
+import './home.scss';
 
 const Home: React.FC = () => {
   return (
-    <div>
+    <div className="MagentaA11y--home-page">
+      <div className="MagentaA11y--home-page__header">
+        <div className="MagentaA11y--home-page__header--title">
+          <h1>Unlock the power of accessibility</h1>
+          <h2>MagentaA11y</h2>
+        </div>
+        <div className="MagentaA11y--home-page__header--logo">
+          <img src={BrandLogo} alt="Brand Logo" />
+        </div>
+      </div>
       <h1>Welcome to MagentaA11y</h1>
       <p>This is the home page.</p>
     </div>
