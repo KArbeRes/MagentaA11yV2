e082486e4a97bd720fdcbd5b0fca9fad5de8d678
KArbela <kevin.arbelaez@t-mobile.com>
2025-03-14 12:20:28 -0700

    Updating document title based on active route

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
-import MyCriteria from 'components/my-criteria/my-criteria';
-import React from 'react';
-import { Route, HashRouter as Router, Routes } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import {
+  Route,
+  HashRouter as Router,
+  Routes,
+  useLocation,
+} from 'react-router-dom';
 import { useCriteria } from 'shared/contexts/criteria-context';
 import { Platforms } from 'shared/types/shared-types';
 import { ReactComponent as BookmarkIconOutlined } from './assets/svgs/bookmark-outlined.svg';
 import About from './components/about-us/about-us';
 import Criteria from './components/criteria/criteria';
 import Home from './components/home/home';
+import MyCriteria from './components/my-criteria/my-criteria';
 import { NavItem } from './components/navigation/nav.types';
 import TopNav from './components/navigation/top-nav/top-nav';
 
 import './App.scss';
 
-const App: React.FC = () => {
+const AppContent: React.FC = () => {
+  const location = useLocation();
   const { savedCriteria } = useCriteria();
-
   const savedCriteriaCount = savedCriteria.length;
 
   const badgeString = savedCriteriaCount
@@ -22,6 +27,31 @@ const App: React.FC = () => {
       ? '99+'
       : `${savedCriteriaCount}`
     : '';
+
+  useEffect(() => {
+    let pathSegments = location.pathname.split('/').filter(Boolean);
+    let pageTitle = '';
+
+    if (pathSegments.length > 0) {
+      let lastSegment = pathSegments[pathSegments.length - 1];
+
+      if (lastSegment.toLowerCase() === 'overview' && pathSegments.length > 1) {
+        lastSegment = pathSegments[pathSegments.length - 2];
+      }
+
+      pageTitle = lastSegment
+        .replace(/-/g, ' ')
+        .replace(/\b\w/g, (c) => c.toUpperCase());
+
+      if (pathSegments.includes('web-criteria')) {
+        pageTitle += ' - Web';
+      } else if (pathSegments.includes('native-criteria')) {
+        pageTitle += ' - Native';
+      }
+    }
+
+    document.title = pageTitle;
+  }, [location]);
 
   const navItems: NavItem[] = [
     { label: 'Web Criteria', href: '/web-criteria' },
@@ -39,29 +69,36 @@ const App: React.FC = () => {
         : '',
     },
   ];
+
+  return (
+    <div className="MagentaA11y">
+      <header className="MagentaA11y-header">
+        <TopNav navItems={navItems} />
+      </header>
+      <div className="MagentaA11y--content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route
+            path="/web-criteria/*"
+            element={<Criteria platform={Platforms.WEB} />}
+          />
+          <Route
+            path="/native-criteria/*"
+            element={<Criteria platform={Platforms.NATIVE} />}
+          />
+          <Route path="/my-criteria" element={<MyCriteria />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+const App: React.FC = () => {
   return (
     <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-      <div className="MagentaA11y">
-        <header className="MagentaA11y-header">
-          <TopNav navItems={navItems} />
-        </header>
-        <div className="MagentaA11y--content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route
-              path="/web-criteria/*"
-              element={<Criteria platform={Platforms.WEB} />}
-            />
-            <Route
-              path="/native-criteria/*"
-              element={<Criteria platform={Platforms.NATIVE} />}
-            />
-            <Route path="/my-criteria" element={<MyCriteria />} />
-          </Routes>
-        </div>
-      </div>
+      <AppContent />
     </Router>
   );
 };
