e5f2dbab389049f4085b7171e2a374274cde688d
Kevin Arbelaez <kevinrestrepo08@gmail.com>
2025-04-11 17:27:17 -0700

    Updated my criteria to active the tab with content if any of them is empty

--- src/components/my-criteria/my-criteria.tsx
+++ src/components/my-criteria/my-criteria.tsx
@@ -1,42 +1,56 @@
-import MarkdownContent from 'components/content-display/markdown-content/markdown-content';
+import MarkdownContent from "components/content-display/markdown-content/markdown-content";
 import {
   ButtonSize,
   ButtonType,
   ButtonVariant,
-} from 'components/custom-components/buttons/button-types';
-import Button from 'components/custom-components/buttons/button/button';
-import Chips from 'components/custom-components/chips/chips';
+} from "components/custom-components/buttons/button-types";
+import Button from "components/custom-components/buttons/button/button";
+import Chips from "components/custom-components/chips/chips";
 import {
   ChipSize,
   ChipType,
   IChipSelectable,
-} from 'components/custom-components/chips/chips.types';
-import Divider from 'components/custom-components/divider/divider';
-import { OrientationEnum } from 'components/custom-components/divider/divider.types';
-import { getFirstOverviewLink } from 'components/navigation/top-nav/top-nav';
-import { useClipboard } from 'hooks/useClipboard';
-import React, { useEffect, useMemo, useRef, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useCriteria } from 'shared/contexts/criteria-context';
-import { Icons } from 'shared/Icons';
-import { DocumentationCategory } from 'shared/types/shared-types';
-import { formatTabLabel } from 'utils/string-helpers';
+} from "components/custom-components/chips/chips.types";
+import Divider from "components/custom-components/divider/divider";
+import { OrientationEnum } from "components/custom-components/divider/divider.types";
+import { getFirstOverviewLink } from "components/navigation/top-nav/top-nav";
+import { useClipboard } from "hooks/useClipboard";
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import { Link } from "react-router-dom";
+import { useCriteria } from "shared/contexts/criteria-context";
+import { Icons } from "shared/Icons";
+import { DocumentationCategory } from "shared/types/shared-types";
+import { formatTabLabel } from "utils/string-helpers";
 
-import '../my-criteria/my-criteria.scss';
+import "../my-criteria/my-criteria.scss";
 
 const MyCriteria: React.FC = () => {
   const { savedCriteria, removeCriteria, clearCriteria } = useCriteria();
   const { copiedContent, copyToClipboard } = useClipboard();
   const [selectedTabIndex, setActiveTab] = useState<number>(0);
   const mergedCriteriaTabs = useMemo(() => {
-    return ['Condensed', 'Gherkin'].map((tabName) => ({
+    return ["Condensed", "Gherkin"].map((tabName) => ({
       tab: tabName,
       content: savedCriteria
         .filter((item) => item.tab === tabName)
         .map((item) => item.content.trim())
-        .join('\n\n---\n\n'),
+        .join("\n\n---\n\n"),
     }));
   }, [savedCriteria]);
+
+  useEffect(() => {
+    const nonEmptyIndex = mergedCriteriaTabs.findIndex(
+      (tab) => tab.content.trim() !== ""
+    );
+    const emptyCount = mergedCriteriaTabs.filter(
+      (tab) => tab.content.trim() === ""
+    ).length;
+
+    if (emptyCount === mergedCriteriaTabs.length - 1 && nonEmptyIndex !== -1) {
+      setActiveTab(nonEmptyIndex);
+    }
+  }, [mergedCriteriaTabs]);
+
   const [deletedChipIndex, setDeletedChipIndex] = useState<number | null>(null);
 
   const tabsElementRef = useRef<HTMLElement>(null);
@@ -45,7 +59,7 @@ const MyCriteria: React.FC = () => {
   const emptyMessageRef = useRef<HTMLDivElement>(null);
 
   const currentTabContent = useMemo(() => {
-    return mergedCriteriaTabs[selectedTabIndex]?.content || '';
+    return mergedCriteriaTabs[selectedTabIndex]?.content || "";
   }, [mergedCriteriaTabs, selectedTabIndex]);
 
   const currentTabChips: IChipSelectable[] = useMemo(() => {
@@ -59,7 +73,7 @@ const MyCriteria: React.FC = () => {
 
   const focusFirstEmptyStateLink = () => {
     if (emptyMessageRef.current) {
-      const firstLink = emptyMessageRef.current.querySelector('a');
+      const firstLink = emptyMessageRef.current.querySelector("a");
       if (firstLink) {
         firstLink.focus();
       }
@@ -98,7 +112,7 @@ const MyCriteria: React.FC = () => {
       selectedTabIndex === 0 ? condensedChipsRef : gherkinChipsRef;
 
     if (updatedChips.length > 0 && refToUse.current) {
-      const chipButtons = refToUse.current.querySelectorAll('button');
+      const chipButtons = refToUse.current.querySelectorAll("button");
       if (chipButtons[nextFocusIndex]) {
         chipButtons[nextFocusIndex].focus();
       }
@@ -111,6 +125,8 @@ const MyCriteria: React.FC = () => {
 
   let criteriaIsCopied =
     copiedContent === mergedCriteriaTabs[selectedTabIndex]?.content;
+
+  console.log({ mergedCriteriaTabs });
 
   return (
     <div className="MagentaA11y__my-criteria">
@@ -126,17 +142,20 @@ const MyCriteria: React.FC = () => {
           <md-tabs
             ref={tabsElementRef}
             aria-label="Criteria options"
-            role="tablist">
+            role="tablist"
+          >
             {mergedCriteriaTabs.map((tab, index) => {
               const formattedLabel = formatTabLabel(tab.tab);
               return (
                 <md-primary-tab
                   key={tab.tab}
-                  aria-selected={selectedTabIndex === index ? 'true' : 'false'}
+                  aria-selected={selectedTabIndex === index ? "true" : "false"}
                   id={`${formattedLabel}-tab`}
                   role="tab"
                   aria-controls={`${formattedLabel}-tabpanel`}
-                  onClick={() => setActiveTab(index)}>
+                  onClick={() => setActiveTab(index)}
+                  {...(selectedTabIndex === index ? { active: true } : {})}
+                >
                   {tab.tab}
                 </md-primary-tab>
               );
@@ -149,7 +168,7 @@ const MyCriteria: React.FC = () => {
               type={ButtonType.button}
               variant={ButtonVariant.primary}
               size={ButtonSize.large}
-              label={criteriaIsCopied ? 'Copied!' : 'Copy Criteria'}
+              label={criteriaIsCopied ? "Copied!" : "Copy Criteria"}
               decoration={criteriaIsCopied ? Icons.checkmark : Icons.copyFilled}
               id="copy-criteria"
             />
@@ -159,7 +178,7 @@ const MyCriteria: React.FC = () => {
 
       {mergedCriteriaTabs.map((criteria, index) => {
         const formattedLabel = formatTabLabel(criteria.tab);
-        const isCondensed = criteria.tab === 'Condensed';
+        const isCondensed = criteria.tab === "Condensed";
 
         return (
           <div
@@ -169,9 +188,10 @@ const MyCriteria: React.FC = () => {
             aria-labelledby={`${formattedLabel}-tab`}
             className={
               selectedTabIndex === index
-                ? 'MagentaA11y__my-criteria__criteria-list'
-                : 'hidden'
-            }>
+                ? "MagentaA11y__my-criteria__criteria-list"
+                : "hidden"
+            }
+          >
             <Chips
               variant={ChipType.BUTTON}
               chips={currentTabChips}
@@ -196,18 +216,19 @@ const MyCriteria: React.FC = () => {
             ) : (
               <div
                 className="MagentaA11y__my-criteria--empty"
-                ref={emptyMessageRef}>
+                ref={emptyMessageRef}
+              >
                 <p>
-                  It seems you haven’t saved any criteria. Start by checking out{' '}
+                  It seems you haven’t saved any criteria. Start by checking out{" "}
                   <Link
-                    to={`${getFirstOverviewLink(DocumentationCategory.WEB)}`}>
+                    to={`${getFirstOverviewLink(DocumentationCategory.WEB)}`}
+                  >
                     Web Criteria
-                  </Link>{' '}
-                  or{' '}
+                  </Link>{" "}
+                  or{" "}
                   <Link
-                    to={`${getFirstOverviewLink(
-                      DocumentationCategory.NATIVE
-                    )}`}>
+                    to={`${getFirstOverviewLink(DocumentationCategory.NATIVE)}`}
+                  >
                     Native Criteria
                   </Link>
                   .
