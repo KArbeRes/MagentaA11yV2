5d82dd05c3551f423a630f71c600ab93ec1d2088
silverli <lindsaysilver1@gmail.com>
2025-04-23 16:05:33 -0400

    feat: new homepage styles

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -7,7 +7,11 @@ const cardContent = [
   {
     "title": "Web accessibility checker",
     "description": "Choose components to define your accessibility success criteria",
+<<<<<<< HEAD
     "link": "/web-criteria/page-level/overview"
+=======
+    "link": "/web-criteria/component/overview"
+>>>>>>> a781038 (feat: new homepage styles)
   },
   {
     "title": "Native accessibility checker",
