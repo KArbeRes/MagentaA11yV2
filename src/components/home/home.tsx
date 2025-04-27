fc32b923ed16bbae62e794e066943a21d4d2c9b1
silverli <lindsaysilver1@gmail.com>
2025-04-27 11:49:37 -0400

    Revert "Adjusted home page background to match rest of app"
    This reverts commit 94bf7b095d6bfef384aa48680902951718130d77.

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,33 +1,30 @@
-import React from "react";
-import TmoLogo from "../../assets/svgs/t-digit-logo.svg";
+import React from 'react';
+import TmoLogo from '../../assets/svgs/t-digit-logo.svg';
 import Cards from "../custom-components/cards/cards";
-import "./home.scss";
+import './home.scss';
 
 const cardContent = [
   {
-    title: "Web accessibility checker",
-    description:
-      "Choose components to define your accessibility success criteria",
-    link: "/web-criteria/component/overview",
+    "title": "Web accessibility checker",
+    "description": "Choose components to define your accessibility success criteria",
+    "link": "/web-criteria/component/overview"
   },
   {
-    title: "Native accessibility checker",
-    description:
-      "Choose components to define your accessibility success criteria",
-    link: "/native-criteria/controls/overview",
+    "title": "Native accessibility checker",
+    "description": "Choose components to define your accessibility success criteria",
+    "link": "/native-criteria/controls/overview"
   },
   {
-    title: "How to test components",
-    description: "Learn how to test for accessibility",
-    link: "/how-to-test-criteria/components/overview",
+    "title": "How to test components",
+    "description": "Learn how to test for accessibility",
+    "link": "/how-to-test-criteria/components/overview"
   },
   {
-    title: "Start adding criteria",
-    description:
-      "Check out your saved criteria and copy them all together at the same time",
-    link: "/my-criteria",
-  },
-];
+    "title": "Start adding criteria",
+    "description": "Check out your saved criteria and copy them all together at the same time",
+    "link": "/my-criteria"
+  }
+]
 
 const Home: React.FC = () => {
   return (
@@ -41,18 +38,19 @@ const Home: React.FC = () => {
           <img src={TmoLogo} alt="T-Mobile logo" />
         </div>
       </div>
-      <div className="MagentaA11y--home-page__content">
-        <div className="text-center">
-          <h2 className="MagentaA11y--home-page__content--header">
-            Get Started
-          </h2>
-          <p>
-            Automatically generate test cases for Web, iOS, and Android
-            components.
-          </p>
+      <div className="MagentaA11y--home-page__content MagentaA11y--home-page__content--beige">
+        <div className='text-center'>
+          <h2 className="MagentaA11y--home-page__content--header">Get Started</h2>
+          <p>Automatically generate test cases for Web, iOS, and Android components.</p>
         </div>
         <Cards items={cardContent} />
       </div>
+      {/* <div className="MagentaA11y--home-page__content MagentaA11y--home-page__content--white">
+        <div className='text-center'>
+          <h2 className="MagentaA11y--home-page__content--header">ARC Summit 2025</h2>
+          <p>Starts May 1, 2025.</p>
+        </div>
+      </div> */}
     </div>
   );
 };
